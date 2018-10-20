0db3dd64de19765a28392d716421cab7d592b816
Riyazulla Sharief <Riyazulla Sharief@RIYAZ-PC>
2018-10-21 02:47:34 +0530

    Update

--- src/scripts/GetText.java
+++ src/scripts/GetText.java
@@ -1,14 +1,11 @@
 package scripts;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
 public class GetText
 {
 	public static void main(String[] args) throws InterruptedException
